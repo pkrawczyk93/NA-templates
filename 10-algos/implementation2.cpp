8747a6ca47ae9f4a65273f663ef07aaf552451ed
Piotr Krawczyk <feniks162433@gmail.com>
2016-11-09 16:02:46 +0100

    koncowy commit

--- 10-algos/implementation2.cpp
+++ 10-algos/implementation2.cpp
@@ -4,14 +4,33 @@
 #include <numeric>
 #include <functional>
 
+/*
+struct type
+{
+};
+
+struct variable
+{
+    struct type * type;
+};
+
+struct function
+{
+    std::vector<variable> arguments;
+    type * return_type;
+};
+*/
+
+//vector<function>, vector<type *>
 type * find_return_type(const std::vector<function> & overload_set, std::vector<type *> argument_types)
 {
-    for (std::vector<function>::const_iterator it = overload_set.begin(); it != overload_set.end(); ++it)
+
+    for (std::vector<function>::const_iterator it = overload_set.begin(); it != overload_set.end(); ++it) // it - aktualnie przegladany function
     {
         bool mismatch = false;
 
-        std::vector<type *>::iterator arg_type_it = argument_types.begin();
-        std::vector<variable>::const_iterator arg_var_it = it->arguments.begin();
+        std::vector<type *>::iterator arg_type_it = argument_types.begin(); // arg_type_it - wskaznik na type
+        std::vector<variable>::const_iterator arg_var_it = it->arguments.begin(); // arg_var_it - przechowuje variable
         for (; arg_var_it != it->arguments.end() && arg_type_it != argument_types.end();
             ++arg_var_it, ++arg_type_it)
         {
--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <utility>
+#include <cstddef>
 
 template<typename T>
-class container_wrapper;
+class container_wrapper
+{
+public:
+ container_wrapper() = default;
+ container_wrapper(T _arg) : _value(std::move(_arg)) 
+ {
+ }
+ container_wrapper(const container_wrapper&) = default;
+ container_wrapper(container_wrapper &&) = default;
+ container_wrapper& operator=(const container_wrapper&) = default;
+ container_wrapper& operator=(const container_wrapper&&) = default;
+  
+ std::size_t size() const;
+ {
+  return _value.size();
+ }
+private:
+ T _value;
+};
 
