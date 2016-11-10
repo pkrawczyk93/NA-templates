2a2b885e4ec1c27ec891754122fcf36e878c621a
Piotr Krawczyk <feniks162433@gmail.com>
2016-11-10 11:53:33 +0100

    commit with template specialization

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -2,6 +2,8 @@
 #include <utility>
 #include <cstddef>
 
+#include <boost/optional.hpp>
+
 template<typename T>
 class container_wrapper
 {
@@ -13,13 +15,34 @@ public:
  container_wrapper(const container_wrapper&) = default;
  container_wrapper(container_wrapper &&) = default;
  container_wrapper& operator=(const container_wrapper&) = default;
- container_wrapper& operator=(const container_wrapper&&) = default;
+ container_wrapper& operator=(container_wrapper&&) = default;
   
- std::size_t size() const;
+ std::size_t size() const
  {
   return _value.size();
  }
 private:
  T _value;
 };
+////////////////////////////
 
+template<typename T>
+class container_wrapper<boost::optional<T> >
+{
+public:
+ container_wrapper() = default;
+ container_wrapper(boost::optional<T> _arg) : _value(std::move(_arg)) 
+ {
+ }
+ container_wrapper(const container_wrapper&) = default;
+ container_wrapper(container_wrapper &&) = default;
+ container_wrapper& operator=(const container_wrapper&) = default;
+ container_wrapper& operator=(container_wrapper&&) = default;
+  
+ std::size_t size() const
+ {
+  return _value ? 1 : 0;
+ }
+private:
+  boost::optional<T> _value;
+};
