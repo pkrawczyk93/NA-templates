2d2617a79937ede208d36d64d1c85f20f754cd80
Piotr Krawczyk <feniks162433@gmail.com>
2016-11-14 11:56:32 +0100

    changes after exercises 51 and 52

--- 50-sfinae/implementation-void_t.h
+++ 50-sfinae/implementation-void_t.h
@@ -9,9 +9,24 @@ struct voider
 template<typename... Ts>
 using void_t = typename voider<Ts...>::type;
 
-template<typename T>
-struct has_type_typedef : std::false_type {};
+template<typename T, typename = void>
+struct has_type_typedef : std::false_type 
+{
+	
+};
+
+
 
 template<typename T>
+struct has_type_typedef<T, void_t<typename T::type> > : std::true_type
+{
+	
+};
+
+template<typename T, typename = void>
 struct has_member_variable : std::false_type {};
+
+template<typename T>
+struct has_member_variable<T, void_t<decltype(std::declval<T>().member)>> : std::true_type{};
+
 
